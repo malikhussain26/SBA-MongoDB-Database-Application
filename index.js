5489da15d7b0d01f0cd407386acd0ece2f10c3a9
malikhussain26 <hussain.am2697@gmail.com>
2024-11-10 02:32:03 -0600

    connected to mongoDB

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Hello World");
+import express from 'express';
+import mongoose from 'mongoose';
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+mongoose.connect('mongodb+srv://mhussain1:rLciETFf1RSgMUP3@cluster0.piptk.mongodb.net/MongoDB-SBA?retryWrites=true&w=majority&appName=Cluster0')
+.then(() => {
+    console.log("Connected to MongoDB!");
+})
+.catch(() => {
+    console.log("Failed to connect to MongoDB!");
+});
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
