01f5a85178b7efc7168552b04f2f7cb0d0e2b616
malikhussain26 <hussain.am2697@gmail.com>
2024-11-12 02:31:04 -0600

    updates

--- index.js
+++ index.js
@@ -1,23 +1,51 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import "dotenv/config";
+const express = require('express');
+const mongoose = require('mongoose');
+require("dotenv/config");
 
 const PORT = process.env.PORT || 3000
+const Car = require('./models/car.model.js');
 const app = express()
 
 app.use(express.json());
 
 
-app.post('/cars', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
-})
+app.post('/cars', async (req, res) => {
+    try {
+        const car = await Car.create(req.body);
+        res.status(200).json(car);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+// update a car
+app.put
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 
-mongoose.connect(process.env.MONGO_URL)
+app.get('/cars', async (req, res) => {
+    try {
+        const cars = await Car.find().then((cars) => {
+            res.status(200).json(cars);
+        })
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+app.get('/car/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const car = await Car.findById(id);
+        res.status(200).json(car);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
+mongoose.connect(process.env.ATLAS_URI)
 .then(() => {
     console.log("Connected to MongoDB!");
 })
