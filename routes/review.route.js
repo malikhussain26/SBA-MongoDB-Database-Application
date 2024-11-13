e29a48df250e1b05ff3698ee2bc018098015dca7
malikhussain26 <hussain.am2697@gmail.com>
2024-11-13 00:00:39 -0600

    fixing routes

--- routes/review.route.js
+++ routes/review.route.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const Car = require('../models/car.model');
+const Review = require('../models/review.model');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
-        const cars = await Car.find().then((cars) => {
-            res.status(200).json(cars);
+        const reviews = await Review.find().then((reviews) => {
+            res.status(200).json(reviews);
         })
     } catch (error) {
         res.status(500).json({message: error.message});
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const Car = require('../models/car.model');
+const User = require('../models/user.model');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
-        const cars = await Car.find().then((cars) => {
-            res.status(200).json(cars);
+        const users = await User.find().then((users) => {
+            res.status(200).json(users);
         })
     } catch (error) {
         res.status(500).json({message: error.message});
